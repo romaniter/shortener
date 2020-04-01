ed5407346981f286b1193a26ea595ef9c3fbe96b
romaniter <rk-proger@mail.ru>
2020-04-01 13:41:42 +0300

    bootstrap styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
         <style>
             html, body {
                 background-color: #fff;
